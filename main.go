956d72a780fbc8cf1dc6c3bb04b05f9a6a064098
Mateusz Fedorowicz <fedorowiczmati@outlook.com>
2024-01-06 18:09:46 +0100

    small update

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+var name = "Foo"
+var firstName string = "Foo"
+
+const (
+	version = 1
+	keyLen  = 10
+)
+
+func main() {
+	name := "foo"
+	fmt.Println(name)
 }
