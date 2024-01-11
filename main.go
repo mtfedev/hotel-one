ae8f32ed3aefcea7b91db7d7fcbf3ff66a1edd68
Mateusz Fedorowicz <fedorowiczmati@outlook.com>
2024-01-11 16:53:55 +0100

    Entity

--- main.go
+++ main.go
@@ -2,20 +2,36 @@ package main
 
 import "fmt"
 
-type User struct {
-	Username string
-	Age      int
+type Position struct {
+	x, y int
+}
+type Entity struct {
+	name    string
+	id      string
+	version string
+
+	Position
 }
 
-func getNumber() int {
-	return 88
+type SpecialEntity struct {
+	Entity
+	specialField float64
 }
 
 func main() {
-	user := User{
-		Username: "Mike",
-		Age:      getNumber(),
+	e := SpecialEntity{
+		specialField: 86.22,
+		Entity: Entity{
+			name:    "my special entity",
+			version: "1.1",
+			Position: Position{
+				x: 23,
+				y: 42,
+			},
+		},
 	}
-
-	fmt.Println("The user is ", user)
+	e.id = "my spcial id"
+	e.x = 234
+	e.y = 2414
+	fmt.Printf("%+v\n", e.id)
 }
