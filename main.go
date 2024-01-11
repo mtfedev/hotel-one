cb46f94d25fc17cba8282086354f8199b7addef5
Mateusz Fedorowicz <fedorowiczmati@outlook.com>
2024-01-11 17:13:42 +0100

    Colors

--- main.go
+++ main.go
@@ -2,36 +2,31 @@ package main
 
 import "fmt"
 
-type Position struct {
-	x, y int
-}
-type Entity struct {
-	name    string
-	id      string
-	version string
+type Color int
 
-	Position
+// fmt.Stringer
+func (c Color) String() string {
+	switch c {
+	case ColorBlue:
+		return "Blue"
+	case ColorBlack:
+		return "Black"
+	case ColorYellow:
+		return "Yellow"
+	case ColorPink:
+		return "Pink"
+	default:
+		panic("invalid color given")
+	}
 }
 
-type SpecialEntity struct {
-	Entity
-	specialField float64
-}
+const (
+	ColorBlue Color = iota
+	ColorBlack
+	ColorYellow
+	ColorPink
+)
 
 func main() {
-	e := SpecialEntity{
-		specialField: 86.22,
-		Entity: Entity{
-			name:    "my special entity",
-			version: "1.1",
-			Position: Position{
-				x: 23,
-				y: 42,
-			},
-		},
-	}
-	e.id = "my spcial id"
-	e.x = 234
-	e.y = 2414
-	fmt.Printf("%+v\n", e.id)
+	fmt.Println("the color is ", ColorBlack)
 }
