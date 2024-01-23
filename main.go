f81b83692745a4ec36691d941a43623d0090e584
Mateusz Fedorowicz <fedorowiczmati@outlook.com>
2024-01-23 21:19:55 +0100

    receiver

--- main.go
+++ main.go
@@ -2,31 +2,24 @@ package main
 
 import "fmt"
 
-type CustomMap[K comparable, V any] struct {
-	data map[K]V
+// if player is not a pointer we are adjusting the copu of the player
+// not the actual player
+type Player struct {
+	HP int
+	// data
 }
 
-func (m *CustomMap[K, V]) Inster(k K, v V) error {
-	m.data[k] = v
-	return nil
-}
-func NewCustomMap[K comparable, V any]() *CustomMap[K, V] {
-	return &CustomMap[K, V]{
-		data: make(map[K]V),
-	}
-}
-
-func cup[T any, B any](val T, x B) {
-	fmt.Println(val)
+// function receiver
+func (p *Player) TakeDamage(amount int) {
+	p.HP -= amount
+	fmt.Println("player is taking damage, New Hp ->", p.HP)
 }
 
 func main() {
+	player := &Player{
+		HP: 100,
+	}
+	player.TakeDamage(10)
 
-	//	m1 := NewCustomMap[string, int]()
-	//	m1.Inster("bar", 1)
-	//	m1.Inster("nor", 2)
-
-	//	m2 := NewCustomMap[int, float64]()
-	//	m2.Inster(2, 9.33)
-	//m2.Inster(3, 100.2313)
+	fmt.Printf("%+v\n", player)
 }
