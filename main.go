8ca820cc134755f38d9f8199041cd2e828d35c21
Mateusz Fedorowicz <fedorowiczmati@outlook.com>
2024-01-25 18:25:58 +0100

    channels

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
-type Player struct {
-	name string
-	hp   int
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// 1 unbuffered channel 0 book
+	// 2 buffered channel 10 books
+	//resultch := make(chan string, 10)  ->buffered channel
+	resultch := make(chan string) // ->unbuffered channel
+	resultch <- "foo"             // -> if now Full ->ITTT WILL BLOCK -> block HERE
+	// this coe below will never execute !!!!
+	result := <-resultch
+	fmt.Println(result)
+
+	//result := fetchResource(1)
+	//fmt.Println("The result:", result)
+	// asynk
+	//go fetchResource()
+
 }
 
-func calculateValues(x int, y int) int {
-	return x + y
-}
-func main() {
+func fetchResource(n int) string {
+	time.Sleep(time.Second * 2)
+	return fmt.Sprintf("result %d", n)
 }
