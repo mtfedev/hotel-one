e822bce243d03a88936a1c31d8567d716acc11ba
Mateusz Fedorowicz <fedorowiczmati@outlook.com>
2024-01-30 19:56:16 +0100

    chanel_server

--- main.go
+++ main.go
@@ -5,29 +5,54 @@ import (
 	"time"
 )
 
-func main() {
-	msgch := make(chan string, 128)
-	msgch <- "A"
-	msgch <- "B"
-	msgch <- "C"
-	close(msgch)
-
-	for {
-		msg, ok := <-msgch
-		if !ok {
-			break
-		}
-		fmt.Println("the message ->", msg)
-	}
-
-	// This piece of code is our consumer
-	//for msg := range msgch {
-	//	fmt.Println("the massage ->", msg)
-	//}
-	//fmt.Println("done reading all the messages from the channel!")
+type Server struct {
+	quitch chan struct{} // 0 bytes
+	msgch  chan string
 }
 
-func fetchResource(n int) string {
-	time.Sleep(time.Second * 2)
-	return fmt.Sprintf("result %d", n)
+func newServer() *Server {
+	return &Server{
+		quitch: make(chan struct{}),
+		msgch:  make(chan string, 128),
+	}
+}
+
+func (s *Server) start() {
+	fmt.Println("server sterting")
+	s.loop() // block
+}
+
+func (s *Server) quit() {
+	s.quitch <- struct{}{}
+}
+func (s *Server) sengMessage(msg string) {
+	s.msgch <- msg
+}
+
+func (s *Server) loop() {
+mainloop:
+	for {
+		select {
+		case <-s.quitch: // do some dtuff when we need to quit
+			fmt.Println("quiting server")
+			break mainloop
+		case msg := <-s.msgch:
+			s.handelMessage(msg) // do some dtuff when we have a message
+		default:
+		}
+	}
+	fmt.Println("server is shutting donw gracefully")
+}
+func (s *Server) handelMessage(msg string) {
+	fmt.Println("we received a message", msg)
+}
+func main() {
+	server := newServer()
+	go func() {
+		time.Sleep(time.Second * 5)
+		server.quit()
+	}()
+
+	server.start()
+
 }
