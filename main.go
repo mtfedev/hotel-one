2fc64faa60e84cf89c2a375879e10337673857dc
Mateusz Fedorowicz <fedorowiczmati@outlook.com>
2024-02-01 22:38:27 +0100

    WaitGroup

--- main.go
+++ main.go
@@ -2,57 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-type Server struct {
-	quitch chan struct{} // 0 bytes
-	msgch  chan string
+type State struct {
+	count int
 }
 
-func newServer() *Server {
-	return &Server{
-		quitch: make(chan struct{}),
-		msgch:  make(chan string, 128),
-	}
-}
-
-func (s *Server) start() {
-	fmt.Println("server sterting")
-	s.loop() // block
-}
-
-func (s *Server) quit() {
-	s.quitch <- struct{}{}
-}
-func (s *Server) sengMessage(msg string) {
-	s.msgch <- msg
-}
-
-func (s *Server) loop() {
-mainloop:
-	for {
-		select {
-		case <-s.quitch: // do some dtuff when we need to quit
-			fmt.Println("quiting server")
-			break mainloop
-		case msg := <-s.msgch:
-			s.handelMessage(msg) // do some dtuff when we have a message
-		default:
-		}
-	}
-	fmt.Println("server is shutting donw gracefully")
-}
-func (s *Server) handelMessage(msg string) {
-	fmt.Println("we received a message", msg)
-}
 func main() {
-	server := newServer()
-	go func() {
-		time.Sleep(time.Second * 5)
-		server.quit()
-	}()
-
-	server.start()
-
+	state := State{}
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		// doing some work
+		go func(i int) {
+			state.count = i + 1
+			wg.Done() // done working
+		}(i)
+	}
+	wg.Wait()
+	fmt.Printf("%+v\n", state)
 }
