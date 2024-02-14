f183a08ccc5b840e743d37e252c4b9f84a2e613a
Mateusz Fedorowicz <fedorowiczmati@outlook.com>
2024-02-14 22:09:19 +0100

    james

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/phuslu/log v1.0.88 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
--- main.go
+++ main.go
@@ -4,23 +4,43 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/mtfedev/hotel-one/api"
-	"github.com/phuslu/log"
+	"github.com/mtfedev/hotel-one/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const dbuei = "mongodb://localhost:27017"
+const dburi = "mongodb://localhost:27017"
+const dbname = "honel-one"
+const userColl = "users"
 
 func main() {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+
+	}
+	ctx := context.Background()
+	coll := client.Database(dbname).Collection(userColl)
+
+	user := types.User{
+		FirstName: "James",
+		LastName:  "at the water cooler",
+	}
+	_, err = coll.InsertOne(ctx, user)
+	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(client)
+
+	var james types.User
+	if err := coll.FindOne(ctx, bson.M{}).Decode(&james); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(james)
 
 	listerAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
 	flag.Parse()
