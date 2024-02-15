a0f178453bab0abee6b0190fdf3b7eb4024b9389
Mateusz Fedorowicz <fedorowiczmati@outlook.com>
2024-02-15 12:03:08 +0100

    user_store

--- api/user_handler.go
+++ api/user_handler.go
@@ -2,17 +2,27 @@ package api
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/mtfedev/hotel-one/db"
 	"github.com/mtfedev/hotel-one/types"
 )
 
-func HandleGetUsers(c *fiber.Ctx) error {
+type UserHadler struct {
+	userStore db.UserStore
+}
+
+func (h *UserHadler) HandleGetUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+	user, err := h.userStore.GetUserByID(id)
+	id err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (h *UserHadler) HandleGetUsers(c *fiber.Ctx) error {
 	u := types.User{
 		FirstName: "James",
 		LastName:  "at the watercooler",
 	}
 	return c.JSON(u)
-}
-
-func HandleGetUser(c *fiber.Ctx) error {
-	return c.JSON("James")
 }
