a04da6a7d91a28258cb2ab240bd278dd0a084c05
Mateusz Fedorowicz <fedorowiczmati@outlook.com>
2024-02-25 23:16:53 +0100

    more tests

--- api/user_handler_test.go
+++ api/user_handler_test.go
@@ -4,8 +4,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
-	"io"
 	"log"
 	"net/http/httptest"
 	"testing"
@@ -44,28 +42,40 @@ func setup(t *testing.T) *testdb {
 func TestPostUser(t *testing.T) {
 	tdb := setup(t)
 	defer tdb.teardown(t)
-	
+
 	app := fiber.New()
 	userHandler := NewUserHandler(tdb.UserStore)
 	app.Post("/", userHandler.HandlePostUser)
 
 	params := types.CreateUserParams{
-		Email: "some@con.com",
+		Email:     "some@con.com",
 		FirstName: "James",
-		LastName: "foo",
-		Password: "iuedgubvhff",
-		
+		LastName:  "foo",
+		Password:  "iuedgubvhff",
 	}
-	b, _:+ json.Marshal(params)
-	req := httptest.NewRequest("POST", "/", nil, bytes.NewReader(b))
+	b, _ := json.Marshal(params)
+	req := httptest.NewRequest("POST", "/", bytes.NewReader(b))
 	req.Header.Add("Content-Types", "application/jason")
-	resp, err:= app.Test(req)
-	if err != nil{
+	resp, err := app.Test(req)
+	if err != nil {
 		t.Error(err)
 	}
-	var user types.User 
-	json.NewDecoder(req.Body).Decode(&user)
+	var user types.User
+	json.NewDecoder(resp.Body).Decode(&user)
+	if len(user.ID) == 0 {
+		t.Errorf("expecting a user id ti be set")
+	}
+	if len(user.EncryptedPassword) > 0 {
+		t.Errorf("expecting the encryptedpassword not be included in the json response")
+	}
+
 	if user.FirstName != params.FirstName {
-		t.Errorf("expexted firstnem %s but got %s", params.FirstName, user.FirstName)
+		t.Errorf("expexted firstname %s but got %s", params.FirstName, user.FirstName)
+	}
+	if user.LastName != params.FirstName {
+		t.Errorf("expexted lastname %s but got %s", params.LastName, user.LastName)
+	}
+	if user.Email != params.Email {
+		t.Errorf("expexted email %s but got %s", params.Email, user.Email)
 	}
 }
