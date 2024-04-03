8b3ef9cd27f09edc807f3ceec2fae9e8b438e00a
Mateusz Fedorowicz <fedorowiczmati@outlook.com>
2024-04-03 23:24:57 +0200

    test 2

--- api/auth_handler_test.go
+++ api/auth_handler_test.go
@@ -2,18 +2,40 @@ package api
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/mtfedev/hotel-one/db"
+	"github.com/mtfedev/hotel-one/types"
 )
+
+func InsertTestUser(t.testing.T,userStore db.UserStore) *types.User {
+	user, err := types.NewUserFromParams(types.CreateUserParams{
+		Email:     "james@wp.pl",
+		FirstName: "james",
+		LastName:  "foo",
+		Password:  "superpassword",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = userStore.InsertUser(context.TODO(), user)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return user 
+}
 
 func TestAuthenticate(t *testing.T) {
 	tdb := setup(t)
 	defer tdb.teardown(t)
+	InsertTestUser(t, tdb.UserStore)
 
 	app := fiber.New()
 	authHandler := NewAuthHandler(tdb.UserStore)
@@ -40,3 +62,5 @@ func TestAuthenticate(t *testing.T) {
 
 	fmt.Println(resp)
 }
+
+//11:00
